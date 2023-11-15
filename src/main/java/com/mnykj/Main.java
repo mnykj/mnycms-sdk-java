52c5ba28b6309cdcd1eba79ec2e38e316733e404
dt_5178266532 <2284612727@qq.com>
2023-11-15 22:29:35 +0800

    初始化提交

---
+++ src/main/java/com/mnykj/Main.java
@@ -0,0 +1,13 @@
+package com.mnykj;
+
+/**
+ * @author 八级天
+ * @blog www.mnykj.com
+ * @email 211983651@qq.com
+ * @since $TIME $DATE
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
